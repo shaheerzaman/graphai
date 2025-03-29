0f997b4c892be0960f2e6357163463b07f843c6d
James Briggs <35938317+jamescalam@users.noreply.github.com>
2025-03-29 20:50:30 +0400

    feat: add tests and updates to func schema

--- graphai/utils.py
+++ graphai/utils.py
@@ -1,11 +1,23 @@
 import inspect
-from typing import Any, Callable, Dict, List, Union, Optional
+from typing import Any, Callable, List, Optional
 from pydantic import BaseModel, Field
+from semantic_router.utils.logger import logger
 
 
 class Parameter(BaseModel):
-    class Config:
-        arbitrary_types_allowed = True
+    """Parameter for a function.
+
+    :param name: The name of the parameter.
+    :type name: str
+    :param description: The description of the parameter.
+    :type description: Optional[str]
+    :param type: The type of the parameter.
+    :type type: str
+    :param default: The default value of the parameter.
+    :type default: Any
+    :param required: Whether the parameter is required.
+    :type required: bool
+    """
 
     name: str = Field(description="The name of the parameter")
     description: Optional[str] = Field(
@@ -15,37 +27,109 @@ class Parameter(BaseModel):
     default: Any = Field(description="The default value of the parameter")
     required: bool = Field(description="Whether the parameter is required")
 
-    def to_openai(self):
+    def to_dict(self) -> dict[str, Any]:
+        """Convert the parameter to a dictionary for an standard dictionary-based function schema.
+        This is the most common format used by LLM providers, including OpenAI, Ollama, and others.
+
+        :return: The parameter in dictionary format.
+        :rtype: dict[str, Any]
+        """
         return {
             self.name: {
                 "description": self.description,
                 "type": self.type,
             }
-        }
+        } 
 
-class FunctionSchema:
+class FunctionSchema(BaseModel):
     """Class that consumes a function and can return a schema required by
     different LLMs for function calling.
     """
-
     name: str = Field(description="The name of the function")
     description: str = Field(description="The description of the function")
     signature: str = Field(description="The signature of the function")
     output: str = Field(description="The output of the function")
-    parameters: List[Parameter] = Field(description="The parameters of the function")
+    parameters: list[Parameter] = Field(description="The parameters of the function")
 
-    def __init__(self, function: Union[Callable, BaseModel]):
-        self.function = function
+    def __init__(
+        self,
+        name: str,
+        description: str,
+        signature: str,
+        output: str,
+        parameters: list[Parameter]
+    ):
+        self.name = name
+        self.description = description
+        self.signature = signature
+        self.output = output
+        self.parameters = parameters
+        
+        
+    @classmethod
+    def from_callable(cls, function: Callable) -> "FunctionSchema":
+        """Initialize the FunctionSchema.
+
+        :param function: The function to consume.
+        :type function: Callable
+        """
         if callable(function):
-            self._process_function(function)
+            name = function.__name__
+            description = str(inspect.getdoc(function))
+            if description is None or description == "":
+                logger.warning(f"Function {name} has no docstring")
+            signature = str(inspect.signature(function))
+            output = str(inspect.signature(function).return_annotation)
+            parameters = []
+            for param in inspect.signature(function).parameters.values():
+                parameters.append(
+                    Parameter(
+                        name=param.name,
+                        type=param.annotation.__name__,
+                        default=param.default,
+                        required=param.default is inspect.Parameter.empty,
+                    )
+                )
+            return cls(
+                name=name,
+                description=description,
+                signature=signature,
+                output=output,
+                parameters=parameters,
+            )
         elif isinstance(function, BaseModel):
             raise NotImplementedError("Pydantic BaseModel not implemented yet.")
         else:
             raise TypeError("Function must be a Callable or BaseModel")
-
+    
+    @classmethod
+    def from_pydantic(cls, model: BaseModel) -> "FunctionSchema":
+        signature_parts = []
+        for field_name, field_model in model.__annotations__.items():
+            field_info = model.model_fields[field_name]
+            default_value = field_info.default
+            if default_value:
+                default_repr = repr(default_value)
+                signature_part = (
+                    f"{field_name}: {field_model.__name__} = {default_repr}"
+                )
+            else:
+                signature_part = f"{field_name}: {field_model.__name__}"
+            signature_parts.append(signature_part)
+        signature = f"({', '.join(signature_parts)}) -> str"
+        return cls(
+            name=model.__class__.__name__,
+            description=model.__doc__ or "",
+            signature=signature,
+            output="",  # TODO: Implement output
+            parameters=[],
+        )
+        
     def _process_function(self, function: Callable):
         self.name = function.__name__
         self.description = str(inspect.getdoc(function))
+        if self.description is None or self.description == "":
+            logger.warning(f"Function {self.name} has no docstring")
         self.signature = str(inspect.signature(function))
         self.output = str(inspect.signature(function).return_annotation)
         parameters = []
@@ -69,15 +153,7 @@ class FunctionSchema:
                 "parameters": {
                     "type": "object",
                     "properties": {
-                        param.name: {
-                            "description": (
-                                param.description
-                                if isinstance(param.description, str)
-                                else "None provided"
-                            ),
-                            "type": self._openai_type_mapping(param.type),
-                        }
-                        for param in self.parameters
+                        param.to_dict() for param in self.parameters
                     },
                     "required": [
                         param.name for param in self.parameters if param.required
@@ -101,31 +177,6 @@ class FunctionSchema:
             return "boolean"
         else:
             return "object"
-
-
-def get_schema_pydantic(model: BaseModel) -> Dict[str, Any]:
-    signature_parts = []
-    for field_name, field_model in model.__annotations__.items():
-        field_info = model.__fields__[field_name]
-        default_value = field_info.default
-
-        if default_value:
-            default_repr = repr(default_value)
-            signature_part = (
-                f"{field_name}: {field_model.__name__} = {default_repr}"
-            )
-        else:
-            signature_part = f"{field_name}: {field_model.__name__}"
-
-        signature_parts.append(signature_part)
-    signature = f"({', '.join(signature_parts)}) -> str"
-    schema = FunctionSchema(
-        name=model.__class__.__name__,
-        description=model.__doc__,
-        signature=signature,
-        output="",  # TODO: Implement output
-    )
-    return schema
 
 DEFAULT = set(["default", "openai", "ollama", "litellm"])
 
---
+++ tests/unit/test_function_schema.py
@@ -0,0 +1,45 @@
+import inspect
+
+from graphai.utils import FunctionSchema
+
+
+def scrape_webpage(url: str, name: str = "test") -> str:
+    """Provides access to web scraping. You can use this tool to scrape a webpage.
+    Many webpages may return no information due to JS or adblock issues, if this
+    happens, you must use a different URL.
+    """
+    return "hello there"
+
+
+def test_function_schema():
+    schema = FunctionSchema(scrape_webpage)
+    assert schema.name == scrape_webpage.__name__
+    assert schema.description == str(inspect.getdoc(scrape_webpage))
+    assert schema.signature == str(inspect.signature(scrape_webpage))
+    assert schema.output == str(inspect.signature(scrape_webpage).return_annotation)
+    assert len(schema.parameters) == 2
+
+
+def test_function_schema_to_dict():
+    schema = FunctionSchema(scrape_webpage)
+    dict_schema = schema.to_dict()
+    assert dict_schema["type"] == "function"
+    assert dict_schema["function"]["name"] == schema.name
+    assert dict_schema["function"]["description"] == schema.description
+    assert dict_schema["function"]["parameters"]["type"] == "object"
+    assert (
+        dict_schema["function"]["parameters"]["properties"]["url"]["type"] == "string"
+    )
+    assert (
+        dict_schema["function"]["parameters"]["properties"]["name"]["type"]
+        == "string"
+    )
+    assert (
+        dict_schema["function"]["parameters"]["properties"]["url"]["description"]
+        is None
+    )
+    assert (
+        dict_schema["function"]["parameters"]["properties"]["name"]["description"]
+        is None
+    )
+    assert dict_schema["function"]["parameters"]["required"] == ["name"]
