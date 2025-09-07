65b33cf39ccdb3f6a554a12f14870fc140366975
Mohammad Shaheer Zaman <shaheer_zaman84@yahoo.com>
2025-09-07 14:09:51 +0530

    adds graph snapshots as in memory persistence

---
+++ graphai/persistence/__init__.py
@@ -0,0 +1,14 @@
+from .in_memory import (
+    EndSnapshot,
+    NodeSnapshot,
+    SimpleStatePersistence,
+    FullStatePersistence,
+)
+
+
+__all__ = [
+    "NodeSnapshot",
+    "EndSnapshot",
+    "SimpleStatePersistence",
+    "FullStatePersistence",
+]
